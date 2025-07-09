ed7d1a73a9cf8f27f98bf5f6718146e2c576c3d4
Edward-Mercado <edwardmercado149@gmail.com>
2025-07-09 10:46:35 -0400

    wait it kinda just works

--- app.py
+++ app.py
@@ -1,11 +1,58 @@
 from flask import Flask, render_template
+import requests
+from datetime import date
+
+API_KEY = "c5da7f6762e05a36fc3391a80e90e947"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 app = Flask(__name__)
+
+def get_weather_data(cityinput): 
+    city = cityinput.replace("_", " ").title()
+    
+    params = {
+    "q": city,
+    "appid": API_KEY,
+    "units": "metric"
+    }
+
+    response = requests.get(BASE_URL, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        city_temp = data['main']['temp']
+        city_weather_description = data['weather'][0]['description']
+        city_humidity = data['main']['humidity']
+        city_wind_speed = data['wind']['speed']
+        city_data = [
+            city,
+            city_temp,
+            city_weather_description,
+            city_humidity,
+            city_wind_speed,
+            date.today(),
+        ]
+        return list(city_data)
+        
+        
+    else:
+        print("fuck you")
 
 @app.route("/")
 def home():
     return render_template('index.html')
-@app.route("/city/city")
-def city_home():
-    print("hi")
-    return render_template('weather.html')
+
+@app.route("/city/<city_name>")
+def city_home(city_name):
+    city_data = get_weather_data(city_name)
+    try:
+        return render_template('weather.html', 
+                           name = city_data[0], 
+                           temp = city_data[1], 
+                           weather_description = city_data[2],
+                           humidity = city_data[3],
+                           wind_speed = city_data[4],
+                           today = city_data[5]
+                           )
+    except TypeError:
+        print('do you think i give a shit about type errors, it works man...')
